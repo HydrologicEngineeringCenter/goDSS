e4ba3f5cc44ffcc1c39bb8c439f8181ace9dfc5c
Will Lehman <william.p.lehman@usace.army.mil>
2025-07-29 00:33:19 +0000

    cleaning up code, improving errors

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ package godss
 import "C"
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -53,11 +54,10 @@ func InitDssFile(filepath string) (DssFile, error) {
 
 	ret := C.hec_dss_open(cFilepath, &cDss)
 
-	fmt.Printf("hec_dss_open returned %d\n", int(ret))
 	if ret != 0 {
 		return file, fmt.Errorf("error opening DSS file, check path and file validity")
 	}
-	fmt.Printf("success! DSS handle = %p\n", cDss)
+
 	ver := C.hec_dss_getVersion(cDss)
 	fmt.Printf("success! DSS version = %v\n", int(ver))
 
@@ -78,7 +78,10 @@ func (d DssFile) ReadCatalog(filter string) (DssCatalog, error) {
 
 	cPathBufferItemSize := C.int(394) //394 defined by  hecdss.c hec_dss_CONSTANT_MAX_PATH_SIZE
 	a := C.hec_dss_catalog(d.fileHandle, cPathBuffer, cRecordTypes, cFilter, cRecordCount, cPathBufferItemSize)
-	fmt.Print(a)
+	if int(a) == 0 {
+		fmt.Println("catalog is empty")
+		return DssCatalog{}, errors.New("empty dss catalog")
+	}
 	stringPathNames := []string{}
 	for i := 0; i < int(cRecordCount); i++ {
 		bdata := pathNames[i*394 : (i+1)*394]
@@ -95,61 +98,21 @@ func (d DssFile) ReadCatalog(filter string) (DssCatalog, error) {
 
 func (d DssFile) ReadTimeSeries(pathname string) error {
 	cPathname := C.CString(pathname)
-
-	/*
-
-
-		C.hec_dss_tsRetrieveInfo(d.fileHandle, cPathname, cUnits, cLength, cType, cLength)
-			//timeLength := 10
-			//startDate := make([]byte, timeLength)
-			//cStartDate := (*C.char)(unsafe.Pointer(&startDate[0]))
-			//startTime := make([]byte, timeLength)
-			//cStartTime := (*C.char)(unsafe.Pointer(&startTime[0]))
-			//cType := (*C.char)(unsafe.Pointer(&mytype[0]))
-			//cLength := C.int(bufferLength)
-	*/
-	cFullSet := C.int(1)
+	cFullSet := C.int(1) //expose as option eventually?
 	cStartDate := C.int(0)
 	cStartTime := C.int(0)
 	cEndDate := C.int(0)
 	cEndTime := C.int(0)
-	//numvals := C.int(0)
-	//qualitySize := C.int(0)
 
 	C.hec_dss_tsGetDateTimeRange(d.fileHandle, cPathname, cFullSet, &cStartDate, &cStartTime, &cEndDate, &cEndTime)
-	fmt.Println(int(cStartDate))
-	fmt.Println(int(cStartTime))
-	fmt.Println(int(cEndDate))
-	fmt.Println(int(cEndTime))
 
-	cInteval := C.int(3600)
-	numPeriods := C.hec_dss_numberPeriods(cInteval, cStartDate, cStartTime, cEndDate, cEndTime)
-	fmt.Println(numPeriods)
-
-	cStartYear := C.int(0)
-	cStartMonth := C.int(0)
-	cStartDay := C.int(0)
-	cEndYear := C.int(0)
-	cEndMonth := C.int(0)
-	cEndDay := C.int(0)
-	C.hec_dss_julianToYearMonthDay(cStartDate, &cStartYear, &cStartMonth, &cStartDay)
-	C.hec_dss_julianToYearMonthDay(cEndDate, &cEndYear, &cEndMonth, &cEndDay)
-	startMonth := time.Month(int(cStartMonth))
-	startTimeGo := time.Date(int(cStartYear), startMonth, int(cStartDay), secondsToHours(int(cStartTime)), 0, 0, 0, time.UTC)
+	startTimeGo := julianToGoTime(cStartDate, cStartTime)
 	startDateString := strings.ToUpper(startTimeGo.Format("02Jan2006"))
 	startTimeString := startTimeGo.Format("15:04:05")
-	fmt.Println(startDateString)
-	endMonth := time.Month(int(cEndMonth))
-	endTimeGo := time.Date(int(cEndYear), endMonth, int(cEndDay), secondsToHours(int(cEndTime)), 0, 0, 0, time.UTC)
-	if endTimeGo.Day() != int(cEndDay) {
-		//subtract 1 minute?
-		endTimeGo = endTimeGo.Add(time.Minute * -1)
-	}
+
+	endTimeGo := julianToGoTime(cEndDate, cEndTime)
 	endDateString := strings.ToUpper(endTimeGo.Format("02Jan2006"))
 	endTimeString := endTimeGo.Format("15:04:05")
-	fmt.Println(endDateString + " " + endTimeString)
-	fmt.Printf("Starting at %v year, %v month, %v day, %v hours\n", int(cStartYear), int(cStartMonth), int(cStartDay), secondsToHours(int(cStartTime)))
-	fmt.Printf("Ending at %v year, %v month, %v day, %v hours\n", int(cEndYear), int(cEndMonth), int(cEndDay), secondsToHours(int(cEndTime)))
 
 	bufferLength := 40
 	units := make([]byte, bufferLength)
@@ -159,43 +122,53 @@ func (d DssFile) ReadTimeSeries(pathname string) error {
 	cLength := C.int(bufferLength)
 
 	timeLength := 10
-	//startDate := make([]byte, timeLength)
-	cStartDateChar := C.CString(startDateString) //(*C.char)(unsafe.Pointer(&startDateString[0]))
+
+	cStartDateChar := C.CString(startDateString)
 	cEndDateChar := C.CString(endDateString)
 	cStartTimeChar := C.CString(startTimeString)
 	cEndTimeChar := C.CString(endTimeString)
 
-	cNumPeriods2 := C.int(0)
+	cNumPeriods := C.int(0)
 	cQualitySize := C.int(0)
-	getSizesErr := C.hec_dss_tsGetSizes(d.fileHandle, cPathname, cStartDateChar, cStartTimeChar, cEndDateChar, cEndTimeChar, &cNumPeriods2, &cQualitySize)
-	fmt.Println(getSizesErr)
-	fmt.Println(int(cNumPeriods2))
-	fmt.Println(int(cQualitySize))
-	//startTime := make([]byte, timeLength)
-	//cStartTimeChar := (*C.char)(unsafe.Pointer(&startTime[0]))
-	//endDate := make([]byte, timeLength)
-	//cEndDateChar := (*C.char)(unsafe.Pointer(&endDateString[0]))
-	//endTime := make([]byte, timeLength)
-	//cEndTimeChar := (*C.char)(unsafe.Pointer(&endTime[0]))
-	times := make([]int, int(cNumPeriods2))
+	getSizesErr := C.hec_dss_tsGetSizes(d.fileHandle, cPathname, cStartDateChar, cStartTimeChar, cEndDateChar, cEndTimeChar, &cNumPeriods, &cQualitySize)
+	if int(getSizesErr) != 0 {
+		return fmt.Errorf("could not determine dimensions of pathname %v", pathname)
+	}
+
+	times := make([]int, int(cNumPeriods))
 	cTimes := (*C.int)(unsafe.Pointer(&times[0]))
-	vals := make([]float64, int(cNumPeriods2))
+	vals := make([]float64, int(cNumPeriods))
 	cVals := (*C.double)(unsafe.Pointer(&vals[0]))
 	cJulian := C.int(0)
-	qualities := make([]int, int(cNumPeriods2))
+	qualities := make([]int, int(cNumPeriods))
 	cQualities := (*C.int)(unsafe.Pointer(&qualities[0]))
-	cArraySize := cNumPeriods2
+	cArraySize := cNumPeriods
 	cGranularity := C.int(3600)
 	timezone := make([]byte, timeLength)
 	cTimezone := (*C.char)(unsafe.Pointer(&timezone[0]))
 	cValsRead := C.int(0)
 	response := C.hec_dss_tsRetrieve(d.fileHandle, cPathname, cStartDateChar, cStartTimeChar, cEndDateChar, cEndTimeChar, cTimes, cVals, cArraySize, &cValsRead, cQualities, cQualitySize, &cJulian, &cGranularity, cUnits, cLength, cType, cLength, cTimezone, cLength)
 	fmt.Println(vals)
-	fmt.Println(response)
+	if int(response) != 0 {
+		return fmt.Errorf("could not read regular timeseries at pathname %v", pathname)
+	}
 	return nil
 }
 func secondsToHours(seconds int) int {
 	return seconds / 60 / 60
+}
+func julianToGoTime(cDate C.int, cTime C.int) time.Time {
+	cYear := C.int(0)
+	cMonth := C.int(0)
+	cDay := C.int(0)
+	C.hec_dss_julianToYearMonthDay(cDate, &cYear, &cMonth, &cDay)
+	Month := time.Month(int(cMonth))
+	Hour := secondsToHours(int(cTime))
+	t := time.Date(int(cYear), Month, int(cDay), Hour, 0, 0, 0, time.UTC)
+	if Hour == 24 {
+		t.Add(time.Nanosecond * -1)
+	}
+	return t
 }
 func (catalog *DssCatalog) RemoveDatesFromCatalog() {
 	for i, p := range catalog.PathNames {
